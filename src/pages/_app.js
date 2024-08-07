badb66835b1abfe0ff9fcff4aaabb21b9051c416
Harshit Shukla <mrharshit2409@gmail.com>
2024-08-07 16:38:15 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }) {
+  return (
+      <div className='main'>
+          <AnimatePresence mode='wait'>
+              <Component key={router.route} {...pageProps} />
+          </AnimatePresence>
+      </div>
+  )
 }
